f04a1a2f31b9c35d7fc7e6a10150407f5c69cd33
tokumaru <tokumaru@symb.io>
2018-09-29 15:49:29 -0700

    Minor context comments on hidden files.

--- .autopep8.py
+++ .autopep8.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# TODO: Explain usage here?  Why do we need both pylint and pep8?
 
 import os
 import subprocess
@@ -10,7 +11,9 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 if path.startswith(current_path + '/project/'):
     path = path.replace(current_path + '/project/', '/src/')
     subprocess.run(['docker-compose', 'exec', '-T', 'web', 'sh', '-c',
-                    "PYTHONPATH=/src/extrasrc autopep8 {} {}".format(" ".join(sys.argv[1:-1]), path)],
+                    "PYTHONPATH=/src/extrasrc autopep8 {} {}".format(" ".join(
+                        sys.argv[1:-1]), path)],
                    stdout=sys.stdout)
 else:
-    subprocess.run([os.environ['HOME'] + '/.local/bin/autopep8'] + sys.argv[1:])
+    subprocess.run(
+        [os.environ['HOME'] + '/.local/bin/autopep8'] + sys.argv[1:])
